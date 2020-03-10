6f964581ff9771d1a27ec1a25df2988013e6edad
itarano <48652904+itarano@users.noreply.github.com>
2020-03-10 18:33:56 +0100

    Update deploy_nifi... to deploy.bat

--- code/deploy/deploy.bat
+++ code/deploy/deploy.bat
@@ -4,6 +4,6 @@ docker inspect itarano/minifi-toolkit:%version% 2> nul 1> nul || docker pull ita
 SET file=%1
 SET path_volume=%cd%
 IF [%file%]==[] (echo Need a file to transform) ELSE (docker run --rm --volume %path_volume%:/templates -it itarano/minifi-toolkit:%version% /opt/minifi-toolkit/bin/config.sh transform /templates/%file:.\=% /templates/config.yml)
-docker rmi minifi-toolkit:%version% 1> nul
+docker rmi itarano/minifi-toolkit:%version% 1> nul
 ECHO.
 IF EXIST "%cd%\config.yml" (ECHO Deployed... && COPY /Y config.yml ..\..\docker\volumes\minifi\conf\templates\ 1>nul && DEL config.yml)
